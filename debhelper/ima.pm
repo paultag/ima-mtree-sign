157d5d12dc8ac7af76f50aace3b56a9833ebb9ed
Paul Tagliamonte <tag@pault.ag>
2017-12-11 23:57:10 -0500

    Add in debhelper hacks

---
+++ debhelper/ima.pm
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+# debhelper addon script for dh-ima
+# written in 2017 by Paul Tagliamonte <paultag@debian.org>
+
+use warnings;
+use strict;
+use Debian::Debhelper::Dh_Lib;
+
+insert_before("dh_gencontrol", "dh_ima");
+
+1
