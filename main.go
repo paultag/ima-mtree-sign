cfd99ddf2c34953e7a23eaa7034dd44796283e96
Paul Tagliamonte <tag@pault.ag>
2017-12-13 00:53:25 -0500

    Add in some debian specific work

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"crypto"
 	"crypto/rand"
@@ -16,35 +17,66 @@ import (
 	"github.com/urfave/cli"
 	"github.com/vbatts/go-mtree"
 
+	"pault.ag/go/debian/control"
+	"pault.ag/go/debian/deb"
 	"pault.ag/go/ima"
 )
 
+func CanonicalizePath(pkg deb.Control, path string) *string {
+	if !strings.HasPrefix(path, "DEBIAN") {
+		return &path
+	}
+
+	var name string
+	switch pkg.MultiArch {
+	case "same", "allowed":
+		name = fmt.Sprintf("%s:%s", pkg.Package, pkg.Architecture)
+	// case "foreign":
+	default:
+		name = pkg.Package
+	}
+
+	base := filepath.Base(path)
+	switch base {
+	case "prerm", "postrm", "preinst", "postinst":
+		el := filepath.Join("/var/lib/dpkg/info", fmt.Sprintf("%s.%s", name, base))
+		return &el
+	default:
+		return nil
+	}
+}
+
 func Main(c *cli.Context) error {
+	fd, err := os.Open(c.GlobalString("control"))
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+	pkg := deb.Control{}
+	if err := control.Unmarshal(&pkg, fd); err != nil {
+		return err
+	}
+
 	key, err := LoadKey(c)
 	if err != nil {
 		return err
 	}
 
-	for _, arg := range c.Args() {
-		if err := os.MkdirAll(filepath.Dir(c.GlobalString("output")), 0755); err != nil {
-			return err
-		}
-
-		dh, err := SignTree(key, rand.Reader, crypto.SHA256, arg)
-		if err != nil {
-			return err
-		}
-
-		fd, err := os.Create(c.GlobalString("output"))
-		if err != nil {
-			return err
-		}
-		defer fd.Close()
-
-		if _, err := dh.WriteTo(fd); err != nil {
-			return err
-		}
+	dh, err := SignTree(pkg, key, rand.Reader, crypto.SHA256, c.GlobalString("root"))
+	if err != nil {
+		return err
 	}
+
+	fd, err = os.Create(c.GlobalString("output"))
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	if _, err := dh.WriteTo(fd); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -66,7 +98,7 @@ func LoadKey(c *cli.Context) (crypto.Signer, error) {
 	return key, nil
 }
 
-func SignTree(signer crypto.Signer, entropy io.Reader, hashFunc crypto.Hash, root string) (*mtree.DirectoryHierarchy, error) {
+func SignTree(pkg deb.Control, signer crypto.Signer, entropy io.Reader, hashFunc crypto.Hash, root string) (*mtree.DirectoryHierarchy, error) {
 	dh := mtree.DirectoryHierarchy{}
 
 	if err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -74,11 +106,16 @@ func SignTree(signer crypto.Signer, entropy io.Reader, hashFunc crypto.Hash, roo
 			return nil
 		}
 
-		entry, err := SignFile(signer, entropy, hashFunc, root, path)
+		entry, err := SignFile(pkg, signer, entropy, hashFunc, root, path)
 		if err != nil {
 			return err
 		}
+		if entry == nil {
+			return nil
+		}
+
 		dh.Entries = append(dh.Entries, *entry)
+
 		return nil
 	}); err != nil {
 		return nil, err
@@ -86,7 +123,7 @@ func SignTree(signer crypto.Signer, entropy io.Reader, hashFunc crypto.Hash, roo
 	return &dh, nil
 }
 
-func SignFile(signer crypto.Signer, entropy io.Reader, hashFunc crypto.Hash, root, path string) (*mtree.Entry, error) {
+func SignFile(pkg deb.Control, signer crypto.Signer, entropy io.Reader, hashFunc crypto.Hash, root, path string) (*mtree.Entry, error) {
 	fd, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -111,8 +148,13 @@ func SignFile(signer crypto.Signer, entropy io.Reader, hashFunc crypto.Hash, roo
 		return nil, err
 	}
 
+	filePath := CanonicalizePath(pkg, relPath)
+	if filePath == nil {
+		return nil, nil
+	}
+
 	return &mtree.Entry{
-		Name: relPath,
+		Name: *filePath,
 		Keywords: []mtree.KeyVal{
 			mtree.KeyVal(fmt.Sprintf("xattr.security.ima=%s", base64.StdEncoding.EncodeToString(sig))),
 		},
@@ -126,7 +168,9 @@ func main() {
 	app.Action = Main
 	app.Flags = []cli.Flag{
 		cli.StringFlag{Name: "privkey"},
-		cli.StringFlag{Name: "output", Value: "mtree"},
+		cli.StringFlag{Name: "root", Value: "."},
+		cli.StringFlag{Name: "output", Value: "DEBIAN/mtree"},
+		cli.StringFlag{Name: "control", Value: "DEBIAN/control"},
 	}
 
 	if err := app.Run(os.Args); err != nil {
